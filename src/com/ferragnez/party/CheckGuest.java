e66e83c55ce8b05984432efea8eeb9314dd1e57c
MarcoAurelioDeFelicis <158585743+MarcoAurelioDeFelicis@users.noreply.github.com>
2024-04-23 03:48:17 +0200

    ADD: bloccato sul ciclo for dell'array lista

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,35 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String [] invitati = {"Dua Lipa" , "Paris Hilton" ,
+				"Manuel Agnelli" , "J-Ax" , "Francesco Totti" ,
+				"Ilary Blasi" , "Bebe Vio" , "Luis" , 
+				"Pardis Zarei" , " Martina Maccherone" ,
+				"Rachel Zeilic" };
+		
+		System.out.println("inserisci il tuo nome e cognome");
+		String nome = scan.nextLine();
+		boolean presente = false;
+		
+		for (int i = 0; i < invitati.length; i++ ) {
+			
+			if (nome == invitati [i]) {
+				System.out.println ("Puoi entrare al party");
+			    break;
+		    }
+			
+		    if (i == invitati.length - 1) {
+				System.out.println("Non puoi entrare alla festa");
+			}
+			
+		}
+	}
+
+}
